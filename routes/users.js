dc2a7056e2c8f68fb2a4aa0af4d5196584e54ab2
pravesh dav <humantorch1932@gmail.com>
2024-07-30 21:03:49 +0530

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import plm from 'passport-local-mongoose';
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }], 
+  fullName: {
+    type: String,
+    require: true
+  },
+  dp: {
+    type: String
+  },
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User', userSchema);
